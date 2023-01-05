8f7d15ad68cb20e9543b15262271d3ff20df937b
Conway <937700753@qq.com>
2023-01-05 14:26:46 -0800

    initial setup

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+// This file is to wrap every page in Layout(Navbar and Footer)
+// And this is in the node environment, so the syntax is a bit different
+
+const React = require("react")
+const Layout = require("./src/components/Layout").default
+
+// element is every page
+exports.wrapPageElement = ({ element, props }) => {
+  return <Layout>{element}</Layout>
+}
