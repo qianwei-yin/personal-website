def4252f4b32e83eb2757e6a8a4ebf5cad136348
Conway <937700753@qq.com>
2023-01-11 17:07:20 -0800

    color change and small screen adjustment

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,33 +11,35 @@ const Projects = ({ title, data }) => {
     <section className="section projects">
       <Title title={title} />
 
-      <div className="section-center type-center">
-        <button
-          className={`${
-            type === "Large-Scale" ? "active" : null
-          } btn btn-primary`}
-          onClick={() => setType("Large-Scale")}
-        >
-          Large-Scale
-        </button>
+      <div className="section-center">
+        <div className="type-center">
+          <button
+            className={`${
+              type === "Large-Scale" ? "active" : null
+            } btn btn-primary`}
+            onClick={() => setType("Large-Scale")}
+          >
+            Large-Scale
+          </button>
 
-        <button
-          className={`${
-            type === "Front-End" ? "active" : null
-          } btn btn-primary`}
-          onClick={() => setType("Front-End")}
-        >
-          Front-End
-        </button>
+          <button
+            className={`${
+              type === "Front-End" ? "active" : null
+            } btn btn-primary`}
+            onClick={() => setType("Front-End")}
+          >
+            Front-End
+          </button>
 
-        <button
-          className={`${
-            type === "Functionality" ? "active" : null
-          } btn btn-primary`}
-          onClick={() => setType("Functionality")}
-        >
-          Functionality
-        </button>
+          <button
+            className={`${
+              type === "Functionality" ? "active" : null
+            } btn btn-primary`}
+            onClick={() => setType("Functionality")}
+          >
+            Functionality
+          </button>
+        </div>
       </div>
 
       <div className="section-center projects-center">
